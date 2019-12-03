41031b716278f10d033a4e89de850d90e6785af1
Ishaq Ali <43699985+johari9292@users.noreply.github.com>
2019-12-04 00:09:08 +0500

    Update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -14,11 +14,11 @@ const Home  =() =>{
 				<br></br>
 				<br></br>
 			  <h1>Welcome to Easy-Ride</h1>
-			  <h2>Let Go Home Early &amp; Save Time &amp; Money Just Registerd Here and Enjoy Our Service </h2>
+			  <h2>Go Home Early &amp; Save Time &amp; Money Just Registered Here and Enjoy Our Service </h2>
 			  <img src="img/hero-img.png" alt="Hero Imgs" />
-			  <a href="https://docs.google.com/forms/d/e/1FAIpQLScZaH2QWE_D-al20TGxWu9GxTKehZI_1ONdxf_gn613VqCZ3g/viewform?usp=sf_link
+			  <a href="https://docs.google.com/forms/d/e/1FAIpQLSfS1uvY9aKcKItEv18tpzBDUfXTTjApZx6MQeojzxiIIHDX0Q/viewform?usp=sf_link
 "  className="btn-get-started scrollto">
-			<h1 style={{fontWeight:'bold'}}>	Let Registerd Here</h1>
+			<h1 style={{fontWeight:'bold'}}>	Registered Here</h1>
 			  </a>
 			
 			</div>
